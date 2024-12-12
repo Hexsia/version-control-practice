51f6e7233119071a58cb62941089732e76fb4451
Hexsia <josearmandohimang@gmail.com>
2024-12-12 19:45:13 +0700

    Add new HTML tags and EJS tags

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", { random: "This text is generated through JS file."})
 })
 
 app.listen(port, console.log(`Server is Live on Port ${port}.`))
